b3eaee595e828dcc45ac97ecfbb634f8022b6465
Mohammad Al Mtar <almtarm@myrobotron.de>
2026-06-02 18:21:01 +0200

    Add weather API demo

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+import requests 
+st.title("Mein projekt startet")
+
+url = "https://api.open-meteo.com/v1/forecast"
+params ={
+    "latitude":52.52,
+    "longitude": 13.41,
+    "current" :"temperature_2m"
+}
+response= requests.get(url, params=params)
+date = response.json()
+
+st.write("Current Date:")
+st.write(date["current"])
+
